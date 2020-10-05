736b7e20557f5c22bb1620622655f5908d26792a
Oscar Bulancea Lindvall <x_oscbu@tetralith1.nsc.liu.se>
2020-10-05 09:24:34 +0200

    small update

--- cythonize/setup.py
+++ cythonize/setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 
 setup(
     name="vaspwfc cythonized",
-    ext_modules=cythonize("VaspBandUnfolding/cythonize/vaspwfc.pyx"),
+    ext_modules=cythonize("VaspBandUnfolding/cythonize/vaspwfc.pyx", build_dir="VaspBandUnfolding/cythonize"),
     zip_safe=False,
 )
