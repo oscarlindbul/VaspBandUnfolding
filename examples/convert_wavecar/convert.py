22c3d360c1086b88a1fa5ab73c63ce5dabaa7bb1
oscarlb <oscar.bulancea.lindvall@liu.se>
2021-03-29 23:32:46 +0200

    Add proper convert script

--- examples/convert_wavecar/convert.py
+++ examples/convert_wavecar/convert.py
@@ -1,8 +1,27 @@
 import sys
-sys.path.append("../../..")
-#from VaspBandUnfolding.vaspwfc import vaspwfc as vaspwfc_normal
+import time
+sys.path.append("../../..") # Add path to library if not in PYTHONPATH
+
+######## Normal Usage
+
+from VaspBandUnfolding.vaspwfc import vaspwfc as vaspwfc_normal
+
+print("Converting wavecar with standard method")
+wfc = vaspwfc_normal("WAVECAR.gamma.ground", lgamma=True, gamma_half="x") #non-compiled
+
+start = time.clock()
+wfc.write_std_wavecar(out="WAVECAR.ground")
+elapsed = time.clock() - start
+print("Time elapsed: {} s".format(elapsed))
+
+######## Compiled Usage
+
 from VaspBandUnfolding.cythonize.vaspwfc import vaspwfc as vaspwfc_c
 
+print("Converting with compiled version")
 wfc = vaspwfc_c("WAVECAR.gamma.ground", lgamma=True, gamma_half="x") # efficient
-#wfc = vaspwfc_normal("data/ground_gamma/WAVECAR.G_gamma.ground", lgamma=True, gamma_half="x") #non-compiled
+
+start = time.clock()
 wfc.write_std_wavecar(out="WAVECAR.ground")
+elapsed = time.clock() - start
+print("Time elapsed: {} s".format(elapsed))
