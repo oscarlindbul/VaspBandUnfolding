0d8073b5f4bff4b806ec1111d128bdbad852bd10
Oscar Bulancea Lindvall <x_oscbu@sigma.nsc.liu.se>
2021-04-30 17:03:19 +0200

    Added E-option to plotipr

--- processing/plot_ipr.py
+++ processing/plot_ipr.py
@@ -9,6 +9,7 @@ parser = arg.ArgumentParser(description="Plots the IPR plot of given defect and 
 
 parser.add_argument("ipr_file", metavar="ipr_file", type=str, help="name of the ipr data file")
 parser.add_argument("--all", dest="plot_all", default=False, action="store_true")
+parser.add_argument("-vsE", dest="versus_E", default=False, action="store_true", help="Plots instead the ipr versus Kohn-Sham energy")
 
 input = parser.parse_args()
 
@@ -25,8 +26,11 @@ if input.plot_all:
 		for spin_channel in range(ipr_data.shape[0]):
 			ipr_channel = ipr_data[spin_channel, k, :, :]
 			bands = np.arange(ipr_channel.shape[0])+1
+			energies = ipr_channel[:,1]
+			x_vals = bands if not input.versus_E else energies
+			bands = np.arange(ipr_channel.shape[0])+1
 			vals = (-1)**(spin_channel)*ipr_channel[:,2]
-			ax.plot(bands, vals, label="spin ch " + str(spin_channel))
+			ax.plot(x_vals, vals, label="spin ch " + str(spin_channel))
 		max_val = np.max(np.abs(vals))
 		test_vals = np.abs(vals) / max_val > 1e-5
 		indices = np.argwhere(test_vals==True)
@@ -45,8 +49,10 @@ else:
 		# take spin channel and average over k-points
 		ipr_channel = ipr_data[spin_channel, 0, :, :]
 		bands = np.arange(ipr_channel.shape[0])+1
+		energies = ipr_channel[:,1]
+		x_vals = bands if not input.versus_E else energies
 		vals = (-1)**(spin_channel)*ipr_channel[:,2]
-		plt.plot(bands, vals, label="spin ch " + str(spin_channel))
+		plt.plot(x_vals, vals, label="spin ch " + str(spin_channel))
 	max_val = np.max(np.abs(vals))
 	test_vals = np.abs(vals) / max_val > 1e-5
 	indices = np.argwhere(test_vals==True)
@@ -56,5 +62,5 @@ else:
 	plt.legend()
 	plt.xlabel("Bands")
 	plt.ylabel("IPR")
-	plt.xlim([x_min, x_max])
+	#plt.xlim([x_min, x_max])
 	plt.show()
